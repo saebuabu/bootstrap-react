b656acfcd17d2ff9302b5351685967ada884a376
jamirojaf <jamiro.jafar@gmail.com>
2024-02-19 16:37:35 +0100

    accordion met een single select

--- src/App.js
+++ src/App.js
@@ -1,103 +1,16 @@
+import React from 'react';
+import Accordion from './components/Accordion'; 
 import './App.css';
-function Example1() {
-  return (
-  <>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        1 of 2
-      </div>
-      <div class="col">
-        2 of 2
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        1 of 3
-      </div>
-      <div class="col">
-        2 of 3
-      </div>
-      <div class="col">
-        3 of 3
-      </div>
-    </div>
-  </div>
-  </>
-  );
-}
-
-function Example2() {
-  return(
-  <>
-  <div class="container text-end">
-    <div class="row justify-content-md-center">
-      <div class="col col-lg-1">
-        1 of 3 row: justify-content-md-center
-      </div>
-      <div class="col-md-auto">
-        Variable width content
-      </div>
-      <div class="col col-lg-1">
-        3 of 3
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-8">
-        1 of 3
-      </div>
-      <div class="col-md-auto">
-        Variable width content
-      </div>
-      <div class="col col-lg-1">
-        3 of 3
-      </div>
-    </div>
-  </div>
-  </>
-  );  
-}
-  
-function Example3() {
-  return (
-  <>
-<div class="container">
-  <div class="row">
-    <div class="col-md-8 col-sm-6">.col-md-8 .col-sm-6</div>
-    <div class="col-6 col-md-4 col-sm-6">.col-6 .col-md-4 col-sm-6</div>
-  </div>
-  <div class="row">
-    <div class="col-6 col-md-4">.col-6 .col-md-4</div>
-    <div class="col-6 col-md-4">.col-6 .col-md-4</div>
-    <div class="col-6 col-md-4">.col-6 .col-md-4</div>
-  </div>
-  <div class="row">
-    <div class="col-6">.col-6</div>
-    <div class="col-6">.col-6</div>
-  </div>
-</div>
-</>
-  );  
-}
-
 
 function App() {
   return (
     <>
     <div className="App">
       <header className="App-header">
-        <h1>Bootstrap Grid System</h1>
+        <h1>Accordion</h1>
       </header>
-      <h2>Example1</h2>
-      <Example1 />
-      <hr />
-      <h2>Example2</h2>
-      <Example2 />
-      <hr />
-      <h2>Example3</h2>
-      <Example3 />
-    </div>
- 
+      <Accordion></Accordion>
+      </div>
     </>
   );
 }
---
+++ src/components/Accordion/data.js
@@ -0,0 +1,22 @@
+/* example accordion data in json format with an id, question and answer */
+const data = [
+  {
+    id: 1,
+    question: "What is React?",
+    answer: "React is a free and open-source JavaScript library for building user interfaces. It is maintained by Facebook and a community of developers. React uses a declarative programming style, which means that you describe what you want the UI to look like, and React will figure out how to make it happen."
+  },
+  {
+    id: 2,
+    question: "What are the benefits of using React?",
+    answer: "There are many benefits to using React, including:\n\n* **Declarative programming:** React uses a declarative programming style, which makes it easy to write and maintain code.\n* **Virtual DOM:** React uses a virtual DOM, which makes it very efficient.\n* **Component-based architecture:** React uses a component-based architecture, which makes it easy to build complex UIs.\n* **Cross-platform:** React can be used to build UIs for web, mobile, and desktop."
+  },
+  {
+    id: 3,
+    question: "How do I use React?",
+    answer: "To use React, you will need to:\n\n1. Install the React library.\n2. Create a new React project.\n3. Write your React code.\n4. Compile your React code.\n5. Deploy your React app."
+  }
+];
+
+
+export default data;
+
