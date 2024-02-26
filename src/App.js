fa0df86001645326799d733217c4dfef0ca61da2
jamirojaf <jamiro.jafar@gmail.com>
2024-02-26 22:56:54 +0100

    Opzetten Opdracht2Kolommen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import Accordion from './components/Accordion'; 
 import './App.css';
+import Opdracht2Kolommen from './components/Opdracht2Kolommen';
+
 
 function App() {
   return (
     <>
     <div className="App">
       <header className="App-header">
-        <h1>Accordion</h1>
+        <h1>2 kolommen</h1>
       </header>
-      <Accordion></Accordion>
+      <Opdracht2Kolommen />
       </div>
     </>
   );
---
+++ src/components/Opdracht2Kolommen/index.jsx
@@ -0,0 +1,18 @@
+import './style.css'
+
+export default function Opdracht2Kolommen() {
+    return (
+        <>
+        <div className="container" id="opdracht2K">
+            <div className="row">
+                <div className="col">
+                    <img src='./img/tangofrankrijk.jpg' alt='Tangoworkshop Frankrijk'></img>
+                </div>
+                <div className="col">
+                    <p>Tweede kolom met dynamische content</p>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+};
